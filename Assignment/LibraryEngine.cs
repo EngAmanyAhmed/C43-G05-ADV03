90c53bf251a2e68a63d8293686dde41fa9852a62
EngAmanyAhmed <eng.amanyahmed.81@gmail.com>
2025-01-31 22:47:22 +0200

    Add project files.

---
+++ Assignment/LibraryEngine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    // User Defined Delegate
+    public delegate string BookFunctionDelegate(Book B);  
+
+    public class LibraryEngine
+    {
+        // a) Method using user-defined delegate  
+        public static void ProcessBooks(List<Book> bList, BookFunctionDelegate fPtr)
+        {
+            foreach (Book B in bList)
+            {
+                Console.WriteLine(fPtr(B));
+            }
+        }
+
+        // b) Method using built-in delegate  
+        public static void ProcessBooksWithFunc(List<Book> bList, Func<Book, string> fPtr)
+        {
+            foreach (Book B in bList)
+            {
+                Console.WriteLine(fPtr(B));
+            }
+        }
+
+        // c) Example usage of anonymous method to get ISBN  
+        public static void ProcessBooksWithAnonymous(List<Book> bList)
+        {
+            ProcessBooks(bList, delegate (Book B) {
+                return B.ISBN;
+            });
+        }
+
+        // d) Example usage of lambda expression to get PublicationDate  
+        public static void ProcessBooksWithLambda(List<Book> bList)
+        {
+            ProcessBooksWithFunc(bList, B => B.PublicationDate.ToShortDateString());
+        }
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part 01
+
+            /* 1. Write Down the Body of all Listed Methods and Properties and Constructor
+               2. You need to parameterize ProcessBooks function to accept BookFunctions Methods using following cases: 
+                  a) Create User Defined Delegate with the same signature of methods existed in Bookfunctions class.
+                  b) Use the Proper build in delegate. 
+                  c) Anonymous Method (GetISBN).
+                  d) Lambda Expression (GetPublicationDate).
+             */
+
+            List<Book> books = new List<Book>
+            {
+                new Book("978-3-16-148410-0", "C# Programming", new[] { "Author One", "Author Two" }, new DateTime(2020, 1, 1), 29.99m),
+                new Book("978-1-56619-909-4", "Advanced C#", new[] { "Author Three" }, new DateTime(2021, 5, 15), 39.99m),
+                new Book("978-0-201-63361-0", "Learning C#", new[] { "Author Four", "Author Five" }, new DateTime(2022, 3, 10), 19.99m)
+            };
+
+            // a) Using user-defined delegate to get titles  
+            LibraryEngine.ProcessBooks(books, BookFunctions.GetTitle);
+
+            // b) Using built-in delegate to get authors  
+            LibraryEngine.ProcessBooksWithFunc(books, BookFunctions.GetAuthors);
+
+            // c) Using anonymous method to get ISBNs  
+            LibraryEngine.ProcessBooksWithAnonymous(books);
+
+            // d) Using lambda expression to get publication dates  
+            LibraryEngine.ProcessBooksWithLambda(books);
+
+            #endregion
+        }
+    }
+}
